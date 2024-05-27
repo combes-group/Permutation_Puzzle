3418256207b127a6563b7b9f7876b350003174b5
Josh Combes <joshua.combes@gmail.com>
2024-05-27 14:49:57 -0600

    adding doc strings

--- Gate_helper.py
+++ Gate_helper.py
@@ -2,6 +2,13 @@ import numpy as np
 import math
 
 def S_gate(dim):
+    """
+    Constructs the Qudit S gate.
+    [TODO: what is the arxiv reference and equation number?]
+
+    :param dim: Hilbert Space dimension
+    :return: numpy array
+    """
     if dim == 2:
         return np.matrix(np.diag([1,1j]))
     omega = np.exp(1j*2*np.pi/dim)
@@ -14,6 +21,13 @@ def S_gate(dim):
     return S_gate
 
 def H_gate(dim):
+    """
+    Constructs the Qudit H gate.
+    [TODO: what is the arxiv reference and equation number?]
+
+    :param dim: Hilbert Space dimension
+    :return: numpy array
+    """
     if dim == 2:
         return 1/np.sqrt(2)*np.matrix([[1,1],[1,-1]])
     omega = np.exp(1j*2*np.pi/dim)
@@ -24,5 +38,12 @@ def H_gate(dim):
     return H
 
 def Z_gate(dim):
+    """
+    Constructs the Qudit Z gate.
+    [TODO: what is the arxiv reference and equation number?]
+
+    :param dim: Hilbert Space dimension
+    :return: numpy array
+    """
     omega = np.exp(1j*2*np.pi/dim)
     return np.matrix(np.diag([omega**n for n in range(dim)]))
